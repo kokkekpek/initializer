6dc9fb24863d959f733750b65ed8f6048f2e5242
Kok Kek <kokkekpek@gmail.com>
2020-06-28 21:47:07 +0300

    Refactoring

--- src/initializer/Initializer.ts
+++ src/initializer/Initializer.ts
@@ -1,7 +1,8 @@
 import PromisesExecutor from './promise/PromisesExecutor'
 import InitializationClearedError from './InitializationClearedError'
+import InitializerInterface from './InitializerInterface'
 
-export default abstract class Initializer {
+export default abstract class Initializer implements InitializerInterface {
     private _initialized: boolean = false
     private _initializationInProgress: boolean = false
     private _clearingInProgress: boolean = false
@@ -9,22 +10,30 @@ export default abstract class Initializer {
     private _clearExecutors: PromisesExecutor = new PromisesExecutor()
 
     public init():Promise<void | Error> {
-        return new Promise(this._initPromise.bind(this))
+        return new Promise((resolve: Function, reject: Function) => {
+            if (this._initialized) {
+                resolve()
+                return
+            }
+            if (this._clearingInProgress) {
+                reject()
+                return
+            }
+            this._initializationExecutors.add(resolve, reject)
+            if (this._initializationInProgress) return
+            this._initializationInProgress = true
+            this._init()
+        })
     }
 
-    private _initPromise(resolve: Function, reject: Function): void {
-        if (this._initialized) {
-            resolve()
-            return
-        }
-        if (this._clearingInProgress) {
-            reject()
-            return
-        }
-        this._initializationExecutors.add(resolve, reject)
-        if (this._initializationInProgress) return
-        this._initializationInProgress = true
-        this._init()
+    public clear():Promise<void | Error> {
+        return new Promise((resolve: Function, reject: Function) => {
+            this._clearExecutors.add(resolve, reject)
+            if (this._clearingInProgress) return
+            this._clearingInProgress = true
+            this._initReject(new InitializationClearedError())
+            this._clear()
+        })
     }
 
     /***********************
@@ -48,18 +57,6 @@ export default abstract class Initializer {
         this._initialized = false
         this._initializationInProgress = false
         this._initializationExecutors.reject(error)
-    }
-
-    public clear():Promise<void | Error> {
-        return new Promise(this._clearPromise.bind(this))
-    }
-
-    private _clearPromise(resolve: Function, reject: Function): void {
-        this._clearExecutors.add(resolve, reject)
-        if (this._clearingInProgress) return
-        this._clearingInProgress = true
-        this._initReject(new InitializationClearedError())
-        this._clear()
     }
 
     /***********************
